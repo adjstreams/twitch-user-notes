0f6487d722b4dc05892d7273e7acb379fd8db08d
ADJStreams <adjstreams@gmail.com>
2025-04-22 13:00:58 +0100

    chore: install commitzen and smenatic release

---
+++ .cz-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    types: [
+      { value: 'feat', name: 'feat:     A new feature' },
+      { value: 'fix', name: 'fix:      A bug fix' },
+      { value: 'docs', name: 'docs:     Documentation only changes' },
+      { value: 'style', name: 'style:    Formatting only' },
+      { value: 'refactor', name: 'refactor: Code change that neither fixes a bug nor adds a feature' },
+      { value: 'test', name: 'test:     Adding or correcting tests' },
+      { value: 'chore', name: 'chore:    Other changes that don’t modify src or test files' }
+    ]
+  };
+  
