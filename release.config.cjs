d6987da9f1ff1f18c0412f01fe538bab80cb3e38
ADJStreams <adjstreams@gmail.com>
2025-04-22 15:54:45 +0100

    fix: js to cjs in buildscript

--- release.config.cjs
+++ release.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd:
-          "node scripts/update-version.js ${nextRelease.version} && node scripts/package-zip.js ${nextRelease.version}"
+          "node scripts/update-version.js ${nextRelease.version} && node scripts/package-zip.cjs ${nextRelease.version}"
       }
     ],
     [
